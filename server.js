f9961d881d11f41ffd8040fc6994c46f322b06bf
vikash-v-creator <vikash.v.official@gmail.com>
2026-04-15 11:57:59 +0530

    fix Express 5 catch-all route crash

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/q/:id', async (req, res) => {
 });
 
 // ── Catch-all: serve frontend for any unmatched route ──
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
